426fbf8cba1c3c338b3c9871013cfd4d2ac8b854
henriquebarros <henriquebarros@live.com>
2024-01-19 20:43:26 -0300

    Fundamentos do NodeJs

---
+++ src/server.js
@@ -0,0 +1,39 @@
+//const http = require('http')
+import http from 'node:http'
+
+
+const users = []
+
+
+const server = http.createServer((req,res)=>{
+    const { method, url } = req;
+
+    if(method==='GET' && url==='/users'){
+        return res.
+               setHeader('Content-type', 'application/json').    
+               end(JSON.stringify(users))
+    }
+
+    if(method==='POST' && url==='/users'){
+        users.push({
+            id:1,
+            name:'John Doe',
+            email:'johndoe@example.com'
+        })
+        return res.writeHead(201).end();
+    }
+
+    return res.writeHead(404)
+})
+
+
+server.listen(3333)
+
+// PADRÕES DE IMPORTAÇÃO
+/*
+    CommonJS => require (default)
+    ESModules => import/export  ("type":"module", alterar no packge.json)
+
+*/
+
+
